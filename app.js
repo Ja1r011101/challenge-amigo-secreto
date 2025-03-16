0b434195b48781c1a6bd9e9f9c00fcfe933032e9
Jair <angelibapa@gmail.com>
2025-03-15 23:58:47 -0600

    Se agregaron las variables globales, la funcion principal y la funcion de borrar campos

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//-----------Inicio de variables globales-----------
+
+let listaAmigos=[];
+
+//-----------Fin de variables globales-----------
+
+
+// -----------Esta es la funcion principal-----------
+function agregarAmigo() {
+    console.log('boton funcionando')
+    let inputUsuario = document.getElementById('amigo'); //esto invoca al input
+    let texto = inputUsuario.value.trim(); //esto corta los espacios y deja solamente el texto
+
+    if (texto === ''){
+        alert('Por favor, agrega un nombre a la lista c:.')
+    } else{
+        if (listaAmigos.includes(texto)){
+            alert(`${texto} ya está en la lista`);
+            borrar();
+        }else{
+            listaAmigos.push(texto);
+            console.log(listaAmigos); 
+            borrar();
+        }
+    }
+}
+
+function borrar(){
+    document.getElementById('amigo').value = '';
+    console.log('funciona borrado')
+}
