3c5f9efdd5f559d3a3cb4ba35a03e78b649273b8
Jair <angelibapa@gmail.com>
2025-03-17 18:34:13 -0600

    Se agrega funcion de mostrar lista y sortear nombres

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //-----------Inicio de variables globales-----------
 
-let listaAmigos=[];
+let arregloAmigos=[];
 
 //-----------Fin de variables globales-----------
 
@@ -12,17 +12,41 @@ function agregarAmigo() {
     let texto = inputUsuario.value.trim(); //esto corta los espacios y deja solamente el texto
 
     if (texto === ''){
-        alert('Por favor, agrega un nombre a la lista c:.')
+        alert('Por favor, agrega un nombre a la lista c:')
     } else{
-        if (listaAmigos.includes(texto)){
+        if (arregloAmigos.includes(texto)){
             alert(`${texto} ya está en la lista`);
             borrar();
         }else{
-            listaAmigos.push(texto);
-            console.log(listaAmigos); 
+            arregloAmigos.push(texto);
+            console.log(arregloAmigos);
+            mostrarNombres();
             borrar();
         }
     }
+}
+
+function sortearAmigo(){
+
+    if (arregloAmigos.length === 0){
+        alert('Aun no hay amigos que sortear');
+    } else{
+        let indiceGanador = Math.floor(Math.random() * arregloAmigos.length);
+        let ganador =arregloAmigos[indiceGanador];
+        let resultado = document.getElementById("resultado");
+
+        resultado.innerHTML =`<li>El ganador es: ${ganador}</li>`
+    }
+}
+
+function mostrarNombres() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ''; //esto evita que la lista sea duplicada en cada uso
+    arregloAmigos.forEach((amigo) => {
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
 }
 
 function borrar(){
